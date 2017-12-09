0984870b53a8d03e76f6397d6f60cc2f080d421a
Artem Demo <artemdemo@gmail.com>
2017-12-09 19:03:09 +0200

    convert cm to feet and inches

--- main.c
+++ main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "myLibrary.h"
-
-#define MAX_LENGTH 21.8
-#define WORK_WEEK 5
-
 int main(void)
 {
-    function1();
+    puts("Enter the height (in cm):");
+
+    float height;
+    scanf("%f", &height);
+
+    float height_inches = height / 2.54;
+    int height_feet = height_inches / 12;
+    double delta_inches = height_inches - (height_feet * 12);
+
+    printf(
+        "Your height is %d feet and %.1f inches.",
+        height_feet,
+        delta_inches
+    );
+
     return EXIT_SUCCESS;
 };
