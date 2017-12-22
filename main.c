d9d4aeafdbbf1f0e059fde3a3705fe7de272de50
Artem Demo <artemdemo@gmail.com>
2017-12-22 21:22:53 +0200

    todo example

--- main.c
+++ main.c
@@ -1,37 +1,16 @@
-/*
- ============================================================================
- Name        : 07_02.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Passing variables to functions, Ansi-style
- 	 	 	   This is an example of functional programming, in which the
- 	 	 	   user interacts with an ATM to debit or credit an account.
- 	 	 	   The program performs 3 transactions. The last two transactions
- 	 	 	   also include adding a monetary gift to the account.
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-// #include "balance.h"
-
-#include "./smth/constants.h"
-
-void printBar(int length);
+#include "./todoModel.h"
 
 int main(void) {
-    printf("%d\n", MAX_VALUE);
-    printBar(10);
-    printBar(5);
-    printBar(23);
-	return EXIT_SUCCESS;
-}
+    initToDoList();
+	addToDo("Some title -0", false);
+	addToDo("Some title -1", true);
+	addToDo("Some title -2", true);
+	struct ToDo someToDo;
 
-void printBar(int length) {
-    for(int i = 0; i < length; i++) {
-        printf("▓");
-    }
-    printf(" %d\n", length);
+	deleteToDo(1);
+	getToDo(1, &someToDo);
+	return EXIT_SUCCESS;
 }
--- smth/constants.h
+++
@@ -1,6 +0,0 @@
-#ifndef CONSTANTS_H_
-#define CONSTANTS_H_
-
-#define MAX_VALUE 100
-
-#endif
